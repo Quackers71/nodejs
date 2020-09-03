174605569320268017aabc300b1b12cf021e434d
quackers <robq71@gmail.com>
2020-09-03 17:32:56 +0100

    file renamed using fs Module

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -32,3 +32,12 @@ fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
     if(err) throw err;
     console.log(data);
 });
+
+// Rename file
+fs.rename(
+    path.join(__dirname, '/test', 'hello.txt'), 
+    path.join(__dirname, '/test', 'helloworld.txt'),
+     (err, data) => {
+    if(err) throw err;
+    console.log('File renamed...');
+});
