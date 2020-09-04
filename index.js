94a7214275931db484ca131aad95ea0ba8464a6d
quackers <robq71@gmail.com>
2020-09-04 18:56:36 +0100

    rest api / micro service

--- index.js
+++ index.js
@@ -14,15 +14,13 @@ const server = http.createServer((req, res) => {
       );
     }
 
-  if (req.url === '/about') {
-      fs.readFile(
-        path.join(__dirname, 'public', 'about.html'),
-        (err, content) => {
-            if (err) throw err;
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(content);
-        }
-      );
+  if (req.url === '/api/users') {
+      const users = [
+          { name: 'Bob Smith', age: 40},
+          { name: 'John Doe', age: 30}
+      ];
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(users));
     }
 });
 
