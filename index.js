14588576e1ffd8c1947efcc7aea96c6022ec7074
quackers <robq71@gmail.com>
2020-09-04 18:50:14 +0100

    added req.url '/about'

--- index.js
+++ index.js
@@ -10,7 +10,19 @@ const server = http.createServer((req, res) => {
             if (err) throw err;
             res.writeHead(200, { 'Content-Type': 'text/html' });
             res.end(content);
-        })
+        }
+      );
+    }
+
+  if (req.url === '/about') {
+      fs.readFile(
+        path.join(__dirname, 'public', 'about.html'),
+        (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        }
+      );
     }
 });
 
