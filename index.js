a075e3987555408bc843189bc2497248d68c41a3
quackers <robq71@gmail.com>
2020-09-04 17:41:06 +0100

    running a very basic server!

--- index.js
+++
@@ -1,9 +0,0 @@
-const Logger = require('./logger');
-
-const logger = new Logger();
-
-logger.on('message', (data) => console.log('Called listener', data));
-
-logger.log('Hello World!');
-logger.log('Hi World!');
-logger.log('Bye World!');
