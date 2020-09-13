4da5920ea4034cba4b174125954a2618e093bd10
robq <robq71@gmail.com>
2020-09-13 21:50:53 +0100

    added Output

--- nodejs-full-tut4begs/app.js
+++ nodejs-full-tut4begs/app.js
@@ -1,3 +1,9 @@
 const tutorial = require('./tutorial');
 
 console.log(tutorial);
+
+// Output
+// $ node app
+// { sum: [Function: sum],
+//   PI: 3.14,
+//   SomeMathObject: [Function: SomeMathObject] }
