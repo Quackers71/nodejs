3a779b499b27e3f1915dfc6931a25d3b90764419
robq <robq71@gmail.com>
2020-09-19 08:53:21 +0100

    extend using the EventEmitter Class

---
+++ nodejs-full-tut4begs/app1.js
@@ -0,0 +1,29 @@
+// The Events Module and EventEmitter Class
+
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('tutorial',(num1,num2)=>{
+    console.log(num1 + num2);
+});
+
+eventEmitter.emit('tutorial',1,2);
+
+class Person extends EventEmitter{
+    constructor(name){
+        super();
+        this._name = name;
+    }
+
+    get name(){
+        return this._name;
+    }
+}
+
+let quackers = new Person('Quackers');
+quackers.on('name',()=>{
+    console.log('My name is ' + quackers.name);
+})
+
+quackers.emit('name');
+
