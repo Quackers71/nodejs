d91435cd5ab88edd3a950dd8e2218225f421c4f1
robq <robq71@gmail.com>
2020-09-20 22:20:55 +0100

    fs.appendFile Method

--- nodejs-full-tut4begs/app3.js
+++ nodejs-full-tut4begs/app3.js
@@ -2,12 +2,19 @@
 
 const fs = require('fs');
 
-fs.rename('example.txt','example2.txt',(err)=>{
+fs.appendFile('example2.txt','\nSome data being appended',(err)=>{
     if(err)
         console.log(err);
     else
-        console.log('Successfully renamed the file!!!');
+        console.log('Successfully appended data to the file!!');
 });
+
+// fs.rename('example.txt','example2.txt',(err)=>{
+//     if(err)
+//         console.log(err);
+//     else
+//         console.log('Successfully renamed the file!!!');
+// });
 
 // //create a file
 // fs.writeFile('example.txt','this is an example',(err)=>{
