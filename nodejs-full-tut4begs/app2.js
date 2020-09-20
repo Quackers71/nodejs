a6842cb60f6d1db80318fead700aa044a155098e
robq <robq71@gmail.com>
2020-09-20 22:02:16 +0100

    using the readline Module

---
+++ nodejs-full-tut4begs/app2.js
@@ -0,0 +1,33 @@
+// Working with the ReadLine Module
+
+const readline = require('readline');
+
+const rl = readline.createInterface({input : process.stdin,
+                         output : process.stdout});
+
+let num1 = Math.floor((Math.random() * 10 ) + 1);
+let num2 = Math.floor((Math.random() * 10 ) + 1);
+let answer = num1 + num2;
+
+rl.question(`What is ${ num1 } + ${ num2 }? \n`,
+(userInput)=>{
+    if(userInput.trim() == answer){
+        rl.close();
+    }
+    else{
+        rl.setPrompt('Incorrect response, please try agin! \n');
+        rl.prompt();
+        rl.on('line',(userInput)=>{
+            if(userInput.trim() == answer)
+                rl.close();
+            else{
+                rl.setPrompt(`Your answer of ${ userInput } is incorrect, try again \n`);
+                rl.prompt();
+            }
+        })
+    }
+});
+
+rl.on('close',()=>{
+    console.log('Correct Yo!!!');
+});
