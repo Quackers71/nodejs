29aa373bd6d3b29c028b31c31a1e731c7db843f3
robq <robq71@gmail.com>
2020-09-20 22:18:13 +0100

    used fs.rename Method

--- nodejs-full-tut4begs/app3.js
+++ nodejs-full-tut4begs/app3.js
@@ -2,16 +2,23 @@
 
 const fs = require('fs');
 
-//create a file
-fs.writeFile('example.txt','this is an example',(err)=>{
+fs.rename('example.txt','example2.txt',(err)=>{
     if(err)
         console.log(err);
     else
-        console.log('File successfully created');
-        fs.readFile('example.txt','utf8',(err,file)=>{
-            if(err)
-                console.log(err);
-            else
-                console.log(file);
-        });
+        console.log('Successfully renamed the file!!!');
 });
+
+// //create a file
+// fs.writeFile('example.txt','this is an example',(err)=>{
+//     if(err)
+//         console.log(err);
+//     else
+//         console.log('File successfully created');
+//         fs.readFile('example.txt','utf8',(err,file)=>{
+//             if(err)
+//                 console.log(err);
+//             else
+//                 console.log(file);
+//         });
+// });
