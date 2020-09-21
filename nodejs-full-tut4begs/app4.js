b40b0168b932d923be93da5e27cb52b54c94c8fd
robq <robq71@gmail.com>
2020-09-21 21:07:54 +0100

    directory not empty, rmdir 'tutorial'

--- nodejs-full-tut4begs/app4.js
+++ nodejs-full-tut4begs/app4.js
@@ -1,14 +1,22 @@
 // Working with the File System Module, pt 2
 
 const fs = require('fs');
-fs.mkdir('tutorial',(err)=>{
+
+fs.rmdir('tutorial',(err)=>{
     if(err)
         console.log(err);
     else
-        fs.writeFile('./tutorial/example.txt','123',(err)=>{
-            if(err)
-                console.log(err);
-            else
-                console.log('Successfully create the file');
-        });        
+        console.log('Folder deleted');
 });
+
+// fs.mkdir('tutorial',(err)=>{
+//     if(err)
+//         console.log(err);
+//     else
+//         fs.writeFile('./tutorial/example.txt','123',(err)=>{
+//             if(err)
+//                 console.log(err);
+//             else
+//                 console.log('Successfully create the file');
+//         });        
+// });
