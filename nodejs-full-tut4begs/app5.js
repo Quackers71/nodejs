e2eb834a5022bd959180a887d25b6fc88c101095
robq <robq71@gmail.com>
2020-09-25 20:40:21 +0100

    working with Streams

---
+++ nodejs-full-tut4begs/app5.js
@@ -0,0 +1,10 @@
+// Working with Readable and Writeable Streams
+
+const fs = require('fs');
+const readStream = fs.createReadStream('./example.txt', 'utf8');
+const writeStream = fs.createWriteStream('example2.txt');
+
+readStream.on('data',(chunk)=>{
+    console.log(chunk);
+    writeStream.write(chunk);
+});
