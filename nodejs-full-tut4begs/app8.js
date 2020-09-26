790afa4ac0d8e1497b98cd45a44dcf37e12c4290
robq <robq71@gmail.com>
2020-09-26 13:53:35 +0100

    serving an example.json file

--- nodejs-full-tut4begs/app8.js
+++ nodejs-full-tut4begs/app8.js
@@ -4,7 +4,7 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer((req,res)=>{
-    const readStream = fs.createReadStream('./static/index.html');
-    res.writeHead(200,{'Content-type': 'text/html'});
+    const readStream = fs.createReadStream('./static/example.json');
+    res.writeHead(200,{'Content-type': 'application/json'});
     readStream.pipe(res);
 }).listen(3000);
