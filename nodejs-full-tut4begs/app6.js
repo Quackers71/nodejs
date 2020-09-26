357357ba526b445b1cb7fb36491891bf0ce85efc
robq <robq71@gmail.com>
2020-09-26 09:37:21 +0100

    using zlib to compress a file into .gzip

--- nodejs-full-tut4begs/app6.js
+++ nodejs-full-tut4begs/app6.js
@@ -1,10 +1,14 @@
 // Pipes and Pipe Chaining
 
 const fs = require('fs');
-const readStream = fs.createReadStream('./example.txt', 'utf8');
-const writeStream = fs.createWriteStream('example2.txt');
+const zlib = require('zlib');
 
-readStream.pipe(writeStream);
+
+const gzip = zlib.createGzip();
+const readStream = fs.createReadStream('./example.txt', 'utf8');
+const writeStream = fs.createWriteStream('example2.txt.gzip');
+
+readStream.pipe(gzip).pipe(writeStream);
 
 // Using pipe is a shorter version than using the writeStream.write method below
 // When using pipe you need two streams i.e. src readStream and dest writeStream
