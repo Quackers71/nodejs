d505fb9ce073bc83e5ec1d76c512bbfe032956d8
robq <robq71@gmail.com>
2020-09-26 13:55:12 +0100

    serving an example.png file

--- nodejs-full-tut4begs/app8.js
+++ nodejs-full-tut4begs/app8.js
@@ -4,7 +4,7 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer((req,res)=>{
-    const readStream = fs.createReadStream('./static/example.json');
-    res.writeHead(200,{'Content-type': 'application/json'});
+    const readStream = fs.createReadStream('./static/example.png');
+    res.writeHead(200,{'Content-type': 'image/png'});
     readStream.pipe(res);
 }).listen(3000);
