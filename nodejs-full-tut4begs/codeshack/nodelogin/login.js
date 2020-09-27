bb2a32230abc568d926614d72f05133543a31dcf
robq <robq71@gmail.com>
2020-09-27 22:07:33 +0100

    Login Form displaying but not fully configured

---
+++ nodejs-full-tut4begs/codeshack/nodelogin/login.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+app.listen(5000);
