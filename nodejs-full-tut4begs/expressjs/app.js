fc32a2ea76b55cf9e3d48a5e8ccd5419e5e87db6
robq <robq71@gmail.com>
2020-09-30 22:02:20 +0100

    Working with JSON Data - Express and Body Parser - still need to fix

--- nodejs-full-tut4begs/expressjs/app.js
+++ nodejs-full-tut4begs/expressjs/app.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use('/public',express.static(path.join(__dirname,'static')));
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'static','login.html'));
@@ -16,7 +17,8 @@ app.get('/',(req,res)=>{
 app.post('/',(req,res)=>{
     console.log(req.body);
     // database work here
-    res.send('Successfuly posted data');
+    res.json({success : true});
+    // res.send('Successfuly posted data');
 });
 
 app.listen(3000);
